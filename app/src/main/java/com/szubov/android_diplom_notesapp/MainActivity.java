472799f1f87af5e4b524d953038aec53bfcefcf1
Sergey Zubov <serjio.zubov@gmail.com>
2020-09-04 16:43:39 +0300

    v0.3

--- app/src/main/java/com/szubov/android_diplom_notesapp/MainActivity.java
+++ app/src/main/java/com/szubov/android_diplom_notesapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.szubov.android_diplom_notesapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
 
+    }
+
+    private void initViews() {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuSettings) {
+
+        }
     }
 }
